7c4b5349a04821f5f67a7f4b6ca7994aef75fe5e
Bob Ziroll <elderziroll@gmail.com>
2023-05-19 15:57:47 -0600

    Fix broken static image assets in build

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,19 +2,31 @@ import React from 'react'
 import data from '../data.json'
 export default function Summary() {
     const items = data.map(item=>{
-        return(
-            
-        <div key={item.id} className= {`summary-details item-${item.id}`}>
-            <div className='left'>
-                <span className='icon'> 
-                <img src={item.icon} alt="" />
-                </span>
-                <span className='category'>{item.category}</span>
+        /**
+         * This is based on the Vite docs page about handling static assets.
+         * https://vitejs.dev/guide/assets.html#new-url-url-import-meta-url
+         * 
+         * There are other ways to accomplish this as well, but this is one way
+         * that works without having to move the images to a different location.
+         * (I.e. a "public" folder, as explained here:
+         * https://vitejs.dev/guide/assets.html#the-public-directory)
+         */
+        const imageUrl = new URL(`../images/${item.icon}`, import.meta.url).href
+        return (
+            <div key={item.id} className={`summary-details item-${item.id}`}>
+                <div className="left">
+                    <span className="icon">
+                        <img src={imageUrl} alt="" />
+                    </span>
+                    <span className="category">{item.category}</span>
+                </div>
+                <div className="">
+                    <span className="marking">
+                        <span className="obtained">{item.score}</span>{" "}
+                        <span className="from"> / 100</span>
+                    </span>
+                </div>
             </div>
-            <div className=''>
-                <span className='marking'><span className='obtained'>{item.score}</span> <span className='from'> / 100</span></span>
-            </div>
-        </div>
         )
     })
   return (
